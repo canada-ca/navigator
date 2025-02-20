e600e7bb93a2ffc8ca788859a5bbbac0ee0d43ca
Max Neuvians <max@neuvians.net>
2025-02-20 14:14:58 +0000

    feat: trim log output

--- valentine/config/prod.exs
+++ valentine/config/prod.exs
@@ -14,7 +14,18 @@ config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Valentine.Finch
 config :swoosh, local: false
 
 # Do not print debug messages in production
-config :logger, :default_handler, formatter: {:logger_formatter_json, %{}}, level: :info
+config :logger, :default_handler,
+  formatter:
+    {:logger_formatter_json,
+     %{
+       template: [
+         :msg,
+         :time,
+         :level,
+         :request_id
+       ]
+     }},
+  level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
